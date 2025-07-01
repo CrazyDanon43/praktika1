710dd20baffe5c2ef985b9f1ed0dce3e3796d766
CrazyDanon43 <118435468+CrazyDanon43@users.noreply.github.com>
2025-07-01 18:49:08 +0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from src.routing.healthcheck_routing import router
 
-app = FastAPI(openai_url="/core/openai.json", docs_url="/core/docs")
+if __name__ == "__main__":
+  app = FastAPI(openai_url="/core/openai.json", docs_url="/core/docs")
 
-app.include_router(router)
+  app.include_router(router)
