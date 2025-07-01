253e012442c24435e3780b08bef79c32ca43e657
CrazyDanon43 <118435468+CrazyDanon43@users.noreply.github.com>
2025-07-01 16:28:48 +0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from src.routing.Healthcheck_rout import router
+from src.routing.healthcheck_routing import router
 
 app = FastAPI(openai_url="/core/openai.json", docs_url="/core/docs")
 
