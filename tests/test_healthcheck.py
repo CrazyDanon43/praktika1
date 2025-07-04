ec8a7004575c5ebf25d342d0f1094cb1f2f37980
CrazyDanon43 <daniilsh43@mail.ru>
2025-07-04 10:28:21 +0300

    асинхронный тест клиент в фикстурах

--- tests/test_healthcheck.py
+++ tests/test_healthcheck.py
@@ -1,9 +1,11 @@
-from fastapi.testclient import TestClient
-from src.app import app
+import pytest
+from src.models.healthcheck_models import HealthStatus
+from httpx import AsyncClient
 
-client = TestClient(app)
-
-def test_healthcheck():
-    response = client.get("/health/healthcheck")
+@pytest.mark.asyncio
+async def test_healthcheck(async_client: AsyncClient):
+    response = await async_client.get("/health/healthcheck")
     assert response.status_code == 200
+    health_status = HealthStatus(**response.json())
+    assert health_status.status is True
     assert response.json() == {"status": True}
